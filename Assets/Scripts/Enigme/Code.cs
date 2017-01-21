08d8ac6bc8be76d7dfca63f47c546f6526f46281
Vincent Dubois <vincent.dubois04@gmail.com>
2017-01-21 16:42:57 +0100

    Enigme fini

---
+++ Assets/Scripts/Enigme/Code.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.EyeTracking;
+
+[RequireComponent(typeof(GazeAware))]
+public class Code : MonoBehaviour {
+
+    private GazeAware _gazeAwareComponent;
+    private bool _isLooked;
+    private bool _isActivate = false;
+
+    // Use this for initialization
+    void Start () {
+        _gazeAwareComponent = GetComponent<GazeAware>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_gazeAwareComponent.HasGazeFocus && !_isLooked)
+        {
+            if (!_isActivate)
+            {
+                Event.Send("code_activated", this);
+            }
+            _isLooked = true;
+        }
+        else if(!_gazeAwareComponent.HasGazeFocus && _isLooked)
+        {
+            _isLooked = false;
+        }
+    }
+
+    public void Activate()
+    {
+        _isActivate = true;
+        GetComponent<MeshRenderer>().material.color = Color.green;
+    }
+
+    public void Disactivate()
+    {
+        _isActivate = false;
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
