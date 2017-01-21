58e091ac195d612553071109f565a0433235823f
Vincent Dubois <vincent.dubois04@gmail.com>
2017-01-21 11:01:22 +0100

    Revert "merge"
    This reverts commit 77fd316ed50999761b45bfbadb3c8311e9fe443b, reversing
    changes made to c1e7e571fd1bc0ec6e053d965c0df4c2a2baee1d.

---
+++ Assets/Scripts/Inputs/FirstPersonController.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+//using UnityStandardAssets.Utility;
+using Random = UnityEngine.Random;
+
+namespace UnityStandardAssets.Characters.FirstPerson
+{
+    [RequireComponent(typeof (CharacterController))]
+    [RequireComponent(typeof (AudioSource))]
+    public class FirstPersonController : MonoBehaviour
+    {
+        [SerializeField] private float m_WalkSpeed;
+        [SerializeField] private MouseLook m_MouseLook;
+
+        private Camera m_Camera;
+        private float m_YRotation;
+        private Vector2 m_Input;
+        private Vector3 m_MoveDir = Vector3.zero;
+        private CharacterController m_CharacterController;
+
+        // Use this for initialization
+        private void Start()
+        {
+            m_CharacterController = GetComponent<CharacterController>();
+            m_Camera = Camera.main;
+			m_MouseLook.Init(transform , m_Camera.transform);
+        }
+
+
+        // Update is called once per frame
+        private void Update()
+        {
+            RotateView();
+        }
+        private void FixedUpdate()
+        {
+            m_MouseLook.UpdateCursorLock();
+        }
+        
+        private void RotateView()
+        {
+            m_MouseLook.LookRotation (transform, m_Camera.transform);
+        }
+    }
+}
